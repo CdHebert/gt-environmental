a026a056668e52f2731702be05a3ee81bf33b198
CBear <chebert222@gmail.com>
2021-09-06 15:30:45 -0700

    added styling to lower half of hompage

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Box, Container, Button } from '@material-ui/core';
+import { makeStyles, Box, } from '@material-ui/core';
 
 const Navbar = (props) => {
 
--- src/pages/hompage.js
+++ src/pages/hompage.js
@@ -16,48 +16,59 @@ const Home = () => {
             {/* <Navbar /> */}
             <div className="whatWeDo">
                 <div className="whatTextContainer">
-                    <h1 className="aboutText">What We Can Do</h1>
-                    <p className="aboutInfo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    {/* <h1 className="aboutText">What We Can Do</h1> */}
+                    <p className="whatText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 {/* <div className="whatAboutPicContainer">
-                    <img className="whatAboutPic" src="/images/placeholder.png" alt="30%" />
+                    <img className="whatAboutPic" src="/images/whatWeDo.png" alt="30%" />
                 </div> */}
             </div>
             <div className="skillsContainer">
                 <div className="img-container">
-                    <img className="skillImages" src="/images/placeholder.png" alt="30%" />
-                    <h4 className="aboutText">What We Sell</h4>
+                    <img className="skillImages" src="/images/fpiLarg.png" alt="30%" />
+                    <h4 className="aboutText2">What We Sell</h4>
                     <p className="skillText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<span><button className="buttonStyles">Learn More</button></span></p>
                 </div>
                 <div className="img-container">
-                    <img className="skillImages" src="/images/placeholder.png" alt="30%" />
-                    <h4 className="aboutText">SCADA</h4>
+                    <img className="skillImages" src="/images/scadaPlaceholder.png" alt="30%" />
+                    <h4 className="aboutText2">SCADA</h4>
                     <p className="skillText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<span><button className="buttonStyles">Learn More</button></span></p>
                 </div>
                 <div className="img-container">
-                    <img className="skillImages" src="/images/placeholder.png" alt="30%" />
-                    <h4 className="aboutText">Services</h4>
+                    <img className="skillImages" src="/images/manholeInstall.png" alt="30%" />
+                    <h4 className="aboutText2">Services</h4>
                     <p className="skillText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<span><button className="buttonStyles">Learn More</button></span></p>
                 </div>
             </div>
             <div className="aboutSection">
-                <div className="aboutContainer">
-                    <div className="aboutTextContainer">
-                        <h1 className="aboutText">About Us</h1>
-                    <div className="aboutPicBox">
-                        <img className="aboutPic" src="/images/placeholder.png" alt="30%" />
-                    </div>
-                        <p className="aboutInfo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five.</p>
+                <div className="aboutContainer1">
+                    <h1 className="aboutText1">What We Do</h1>
+                    <div className="aboutTextContainer1">
+                        {/* <div className="aboutPicBox1">
+                            
+                            <i class="far fa-check-square"></i>
+                            <i class="far fa-check-square"></i>
+                            <i class="far fa-check-square"></i>
+                            <i class="far fa-check-square"></i>
+                            <i class="far fa-check-square"></i>
+                        </div> */}
+                        <div className="aboutInfo1">
+                            <p><i class="far fa-check-square"></i> Lorem Ipsum is simply dummy text of the printing</p>
+                            <p><i class="far fa-check-square"></i> Lorem Ipsum is simply dummy text of the printing</p>
+                            <p><i class="far fa-check-square"></i> Lorem Ipsum is simply dummy text of the printing</p>
+                            <p><i class="far fa-check-square"></i> Lorem Ipsum is simply dummy text of the printing</p>
+                            
+                        </div>
                     </div>
 
                 </div>
-                <div className="aboutContainer">
-                    <div className="aboutTextContainer">
-                        <h1 className="aboutText">Who We Are</h1>
-                    <div className="aboutPicBox">
-                        <img className="aboutPic" src="/images/placeholder.png" alt="30%" />
-                    </div>
-                        <p className="aboutInfo">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.</p>
+                <div className="aboutContainer2">
+                    <div className="aboutTextContainer2">
+                        <h1 className="aboutText1">Who We Are</h1>
+                        <div className="aboutPicBox2">
+                            <img className="aboutPic2" src="/images/aboutUs.jpg" alt="30%" />
+                        </div>
+                        <p className="aboutInfo2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to.</p>
                     </div>
                 </div>
             </div>
