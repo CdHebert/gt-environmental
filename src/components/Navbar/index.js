8bd4dc25779655118187310abd13386555ba1b6b
CBear <chebert222@gmail.com>
2021-09-20 14:35:43 -0700

    nav bar final media query

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,66 +1,14 @@
 import React from 'react';
-import { makeStyles, Box, } from '@material-ui/core';
+
 
 const Navbar = (props) => {
 
-
-
-    const useStyles = makeStyles({
-        navContainer: {
-            display: "flex",
-            flexDirection: "row",
-            color: "white",
-            backgroundImage: "url(/images/stoneBg.jpg)",
-            // backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            width: "100%",
-        },
-        colorHeader: {
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            alignItems: "center",
-            alignSelf: "center",
-            justifyContent: "center",
-            textDecoration: "none",
-            maxWidth: "15%",
-            margin: "20px"
-        },
-        navButtonsContainer: {
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            width: "65%",
-            textDecoration: "none"
-        },
-        imageContainer: {
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            width: "15%",
-        },
-        imageStyle: {
-           alignSelf: "flex-start",
-            width: "100%",
-    
-        },
-    
-    })
-
-    const classes = useStyles();
-
     const tabs = ['Home', 'Flow-Meters', 'SCADA', 'Services']
     return (
-        <nav id='nav-wrap' className={classes.navContainer}>
+        <nav id='nav-wrap' className="navContainer">
 
-            <h1 style={{
-                fontSize: "28px",
-                // color: "#2e65b8"
-            }} className={classes.colorHeader}>GT Environmental</h1>
-            <ul id='nav' className={classes.navButtonsContainer}>
+            <h1 className="colorHeader">GT Environmental</h1>
+            <ul id='nav' className="navButtonContainer">
                 {tabs.map(tab => (
                     <li className="navApp" key={tab}>
                         <a className='navApp'
@@ -75,10 +23,10 @@ const Navbar = (props) => {
                     </li>
                 ))}
             </ul>
-            <Box className={classes.imageContainer}>
-                <img className={classes.imageStyle} alt="mccrometer logo" src="/images/Crom.png" />
+            <div className="mccromBox">
+                <img className="imgStyle" alt="mccrometer logo" src="/images/Crom.png" />
                 <h5 className="authorized">Authorized Dealer</h5>
-            </Box>
+            </div>
             
         </nav>
     )
