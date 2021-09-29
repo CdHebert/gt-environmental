998ed3f0f357e6bd37b28addec0d02e9666aef3e
CBear <chebert222@gmail.com>
2021-09-29 16:40:08 -0700

    small fixes to SCADA

--- src/pages/SCADA.js
+++ src/pages/SCADA.js
@@ -1,31 +1,37 @@
 import React from "react";
+import ReactPlayer from 'react-player'
 
 const SCADA = () => {
-return (
-    <section style={{display: "flex",
-    flexDirection: "column"}}>
-<h1 style={{
-    display: "flex",
-    alignSelf: "center",
-    // alignContent: "center",
-    // alignItems: "center",
-    // justifyContent: "center",
-    fontSize: "50px"
-}}>Work In Progress Come Back Later For New Content!</h1>
-<img src="/images/wireing.jpg" alt="one worker installing a flow meter" style={{
-    width: "80%",
-    height: "100%",
-    display: "flex",
-    alignSelf: "center",
-    border: "solid 5px cadetblue",
-    borderRadius: "15px",
-    boxShadow: "5px 5px 10px",
-    // alignContent: "center",
-    // alignItems: "center",
-    // justifyContent: "center",
-}}></img>
-</section>
-)
+    return (
+        <section style={{
+            display: "flex",
+            flexDirection: "column"
+        }}>
+            <div className="thisvideo">
+<ReactPlayer url="https://youtu.be/https://www.youtube.com/watch?v=m9wdYy3tCm4" controls playbackRate = {2} width = "65%" height = "504px" alignSelf="center"/>
+</div>
+            <h1 style={{
+                display: "flex",
+                alignSelf: "center",
+                // alignContent: "center",
+                // alignItems: "center",
+                // justifyContent: "center",
+                fontSize: "50px"
+            }}>Work In Progress Come Back Later For New Content!</h1>
+            <img src="/images/wireing.jpg" alt="one worker installing a flow meter" style={{
+                width: "80%",
+                height: "100%",
+                display: "flex",
+                alignSelf: "center",
+                border: "solid 5px cadetblue",
+                borderRadius: "15px",
+                boxShadow: "5px 5px 10px",
+                // alignContent: "center",
+                // alignItems: "center",
+                // justifyContent: "center",
+            }}></img>
+        </section>
+    )
 }
 
 export default SCADA;
