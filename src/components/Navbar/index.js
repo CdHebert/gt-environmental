3e9da492beae6114ea5c6b6c762928ab8534c00f
CBear <chebert222@gmail.com>
2021-12-28 14:35:56 -0700

    added image slider to services

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,24 +14,13 @@ const Navbar = () => {
                     <img className="headerLogo" alt="gt environmental logo" src="/images/GT12.png" />
                 </Link><span id="home" className="hidden-home "><i className="fas fa-hand-point-left"></i> Click to go Home!</span>
             </div>
-            <ul id='nav' className="navButtonContainer">
-                <Link className="navApp hvr-pulse-grow" to='/meters'>Flow Meters</Link>
-                <Link className="navApp hvr-pulse-grow" to='/scada'>SCADA</Link>
-                <Link className="navApp hvr-pulse-grow" to='/services'>services</Link>
-                {/* {tabs.map(tab => (
-                    <li className="navApp hvr-pulse-grow" key={tab}>
-                        <a className='navApp'
-                            href={'#' + tab.toLowerCase()}
-                            // Whenever a tab is clicked on,
-                            // the current page is set through the handlePageChange props.
-                            onClick={() => props.handlePageChange(tab)}
 
-                        >
-                            {tab}
-                        </a>
-                    </li>
-                ))} */}
+            <ul id='nav' className="navButtonContainer">
+                <Link className="navApp hvr-pulse-grow" to='/meters'><li>Flow Meters</li></Link>
+                <Link className="navApp hvr-pulse-grow" to='/scada'><li>SCADA</li></Link>
+                <Link className="navApp hvr-pulse-grow" to='/services'><li>Services</li></Link>
             </ul>
+            
             <div className="mccromBox">
                 <img className="imgStyle" alt="mccrometer logo" src="/images/Crom.png" />
                 <h5 className="authorized">Authorized Dealer</h5>
---
+++ src/components/imageSlider/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { SliderData } from './sliderData'
+import { GoArrowLeft, GoArrowRight } from 'react-icons/go'
+
+
+const ImageSlider = ({ slides }) => {
+    const [currentImg, setCurrentImg] = useState(0)
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1)
+    }
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    return (
+        <section className='pipe-container'>
+            <GoArrowLeft className='left-arrow' onClick={prevSlide} />
+            <GoArrowRight className='right-arrow' onClick={nextSlide} />
+            {SliderData.map((images, index) => {
+                return (
+                    <div 
+                    className={index === currentImg ? 'slide active' : 'slide'} 
+                    key={index} 
+                    >
+                        {index === currentImg && (
+                            <img src={images.image} alt={images} className='pipe-img' />
+                        )}
+                    </div>
+                )
+            })}
+        </section>
+
+    )
+}
+
+export default ImageSlider;
