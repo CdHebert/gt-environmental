48501045ec100e3ef5cdbd5742339289ab7365c9
CBear <chebert222@gmail.com>
2022-01-06 15:57:20 -0700

    flip card added

--- src/components/WiringSlider/index.js
+++ src/components/WiringSlider/index.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { WiringData } from '../imageSlider/sliderData'
-// import { GoArrowLeft, GoArrowRight } from 'react-icons/go'
+import { useSpring, a } from '@react-spring/web'
+
+import styles from '../../assets/css/styles.module.css'
+
 
 
 const WireSlider = ({ slides }) => {
     const [currentWire, setCurrentWire] = useState(0)
     const length = slides.length;
     const timeoutRef = useRef(null)
-    const delay = 3500;
+    const delay = 55500;
 
     function resetTimeout() {
         if (timeoutRef.current) {
@@ -29,23 +32,64 @@ const WireSlider = ({ slides }) => {
             resetTimeout();
         }
     })
-    return (
-        <div className='wire-container'>
-            {WiringData.map((wire, index) => {
-                return (
-                    <div
-                        className={index === currentWire ? 'newSlide reactive' : 'newSlide'}
-                        key={index}
-                    >
-                        {index === currentWire && (
-                            <img src={wire.images} alt={wire} className='wire-img' />
-                        )}
-                    </div>
 
-                )
-            })}           
-                <div className="slideshowDots">
-                    {WiringData.map((_, idx) => (
+    const [flipped, set] = useState(false)
+    const { transform, opacity } = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 },
+    })
+
+
+    return (
+        // <div className={styles.container} onClick={() => set(state => !state)}>
+
+        /* <a.div
+            className={`${styles.c} ${styles.front}`}
+            style={{
+                opacity,
+                transform,
+                rotateX: '180deg',
+            }}
+        > */
+
+
+
+        < div onClick={() => set(state => !state)} className={`wire-container ${styles.container}`} >
+            <a.div
+                className={`${styles.c} ${styles.back}`}
+                style={{ opacity: opacity.to(o => 1 - o), transform }}
+            >
+                <h2>Hello and Welcome</h2>
+                <p>this is what we offer with our wire work on panels you can expect great things</p>
+                </a.div>
+            {
+                WiringData.map((wire, index) => {
+                    return (
+                        <a.div
+                            // className={``}
+                            style={{
+                                opacity,
+                                transform,
+                                rotateX: '180deg',
+                            }}
+                            className={index === currentWire ? ` ${styles.front} newSlide reactive ` : `newSlide`}
+                            key={index}
+                        >
+                            {index === currentWire && (
+                                <img src={wire.images} alt={wire} className='wire-img' />
+                            )}
+
+                        </a.div>
+                    )
+                })
+            }
+
+            {/* </div > */}
+            {/* </a.div> */}
+            < div className="slideshowDots" >
+                {
+                    WiringData.map((_, idx) => (
                         <div
                             key={idx}
                             className={`slideshowDot${currentWire === idx ? " active" : ""}`}
@@ -53,10 +97,10 @@ const WireSlider = ({ slides }) => {
                                 setCurrentWire(idx);
                             }}
                         ></div>
-                    ))}
-                </div>
-        </div>
-
+                    ))
+                }
+            </div >
+        </div >
     )
 }
 
